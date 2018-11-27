bde239fbcb7fa90bc5e46bda9d7a7800c458e765
Ratika Arora <ratikaarora22@gmail.com>
2018-11-27 15:34:20 +0530

    Ratika: My new files

---
+++ month.rb
@@ -0,0 +1,33 @@
+puts "enter a"
+a=gets.chomp
+m=a.to_i
+puts m
+case m
+when m=1
+puts "jan"
+when m =2
+puts "feb"
+when m =3
+puts "mar"
+when m =4
+puts "april"
+when m=5
+puts "may"
+when m=6
+puts "june"
+when m =7
+puts "july"
+when m=8
+puts "aug"
+when m=9
+puts "sep"
+when m =10
+puts "oct"
+when m=11
+puts "nov"
+when m=12
+puts "dec"
+else
+puts "out of range"
+end
+
---
+++ months.rb
@@ -0,0 +1,7 @@
+a=Array[1,2,3]
+b=Array[4,2,3]
+if months[m].nil?
+puts "garbage value"
+end
+
+ 
